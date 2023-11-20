cd17b5f558a7f49422cd33289ab8851f4ae89ad7
chinhbean09 <chinh0726@gmail.com>
2023-11-21 01:06:29 +0700

    Set Up Eureka Server

---
+++ src/main/java/com/chinhbean/bookservice/BookServiceApplication.java
@@ -0,0 +1,15 @@
+package com.chinhbean.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
+
+}
