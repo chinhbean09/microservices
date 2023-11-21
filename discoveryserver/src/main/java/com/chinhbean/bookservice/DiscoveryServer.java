3118bde4bd79cb6c8c0725a78c98e9a416c46fee
chinhbean09 <chinh0726@gmail.com>
2023-11-21 23:57:49 +0700

    Add bookservice

--- discoveryserver/src/main/java/com/chinhbean/bookservice/DiscoveryServer.java
+++ discoveryserver/src/main/java/com/chinhbean/bookservice/DiscoveryServer.java
@@ -2,11 +2,10 @@ package com.chinhbean.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class DiscoveryServer {
 
     public static void main(String[] args) {
