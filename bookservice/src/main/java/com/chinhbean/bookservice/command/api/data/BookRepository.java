2c661ec42c3a154387fb1f7e2f3ba8c4ebb930a4
chinhbean09 <chinh0726@gmail.com>
2024-02-26 21:51:48 +0700

    Revert "Feature: Create Book"
    This reverts commit d4a601429d2bfaa74a65e6f0aa21473f0973ec49.

--- bookservice/src/main/java/com/chinhbean/bookservice/command/api/data/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chinhbean.bookservice.command.api.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String>{
-
-}
